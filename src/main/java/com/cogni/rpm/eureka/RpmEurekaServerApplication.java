5e9b5983326b970efc5ed675203fd65577558331
Swapnil <murkute.swapnil12@gmail.com>
2021-06-16 19:40:10 +0530

    swapnil first commit

---
+++ src/main/java/com/cogni/rpm/eureka/RpmEurekaServerApplication.java
@@ -0,0 +1,15 @@
+package com.cogni.rpm.eureka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class RpmEurekaServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RpmEurekaServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cogni/rpm/eureka/RpmEurekaServerApplicationTests.java
@@ -0,0 +1,17 @@
+package com.cogni.rpm.eureka;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RpmEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+		assertTrue(true);
+	}
+
+}
